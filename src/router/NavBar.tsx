1115fb30277cd2a1e07548b53865ec3a3604fa04
ArnulfoVargas <vargas.mejia.arnulfo04@gmail.com>
2023-10-02 02:03:27 -0600

    NavBar

--- src/router/NavBar.tsx
+++ src/router/NavBar.tsx
@@ -1,23 +1,35 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom'
 
 
 const NavBar = () => {
 
-
+const [showHamburguer, setShowHamburguer] = useState<boolean>(false)
   return (
-      <div className="h-20 bg-gray-800 flex flex-row px-5 justify-between items-center z-50">
-      <div className="flex flex-row h-full items-center justify-center w-16">
+      <div className={showHamburguer  ? "xs:h-80 md:h-20 bg-gray-800 flex flex-row px-5 justify-between items-center sticky top-0 z-50 transition-all duration-200" 
+                                      : "xs:h-20 md:h-20 bg-gray-800 flex flex-row px-5 justify-between items-center sticky top-0 z-50 transition-all duration-200"}>
+                                        
+      <div className="flex flex-row h-20 items-center justify-center w-16 xs:fixed xs:top-0 md:static">
           <Link to={"/home"}>
             <img src="/LogoTransparenteBlanco.png" alt="Arnulfo Logo" className='h-14'/>
           </Link>
       </div>
 
-      <div className='md:hidden xs:flex xs:h-16 xs:w-16 items-center justify-center'>
-        <svg viewBox="0 0 100 80" width="40" height="40" className='fill-gray-200'>
-          <rect width="100" height="20" rx="3"></rect>
-          <rect y="30" width="100" height="20" rx="3"></rect>
-          <rect y="60" width="100" height="20" rx="3"></rect>
-        </svg>
+      <div className='md:hidden xs:flex xs:h-full xs:w-full items-end justify-end'>
+        <div className='h-20 w-10 fixed top-0 flex items-center' onClick={() => {setShowHamburguer(!showHamburguer)}}>
+          <svg viewBox="0 0 100 80" width="40" height="40" className='fill-gray-200'>
+            <rect width="100" height="20" rx="3"></rect>
+            <rect y="30" width="100" height="20" rx="3"></rect>
+            <rect y="60" width="100" height="20" rx="3"></rect>
+          </svg>
+        </div>
+
+        <div className={showHamburguer ? "xs:flex flex-col items-end gap-3 md:hidden mb-10" : "hidden"}>
+            <Link to={"/home"}> <div className='text-xl font-bold text-gray-200 flex items-center justify-center h-10'><p>Home</p></div> </Link>
+            <Link to={"/about"}> <div className='text-xl font-bold text-gray-200 flex items-center justify-center h-10'><p>About</p></div> </Link>
+            <Link to={"/pokeapi"}> <div className='text-xl font-bold text-gray-200 flex items-center justify-center h-10'><p>PokeAPI</p></div> </Link>
+            <Link to={"/contact"}> <div className='text-xl font-bold text-gray-200 flex items-center justify-center h-10'><p>Contact</p></div> </Link>
+        </div>
       </div>
 
       <div className='items-center gap-5 flex-row xs:hidden md:flex'>
