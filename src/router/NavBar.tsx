9e3d4844e9802ad69a180193cf7661289aa891be
ArnulfoVargas <vargas.mejia.arnulfo04@gmail.com>
2023-10-03 17:43:26 -0600

    more functionality

--- src/router/NavBar.tsx
+++ src/router/NavBar.tsx
@@ -1,16 +1,36 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 const NavBar = () => {
+
+  const [open, setOpen] = useState<boolean>(false)
+  const body = document.querySelector("body");
+
+  const onClickEvent = ()=>{
+    setOpen(!open);
+
+    if (open === true){
+      body?.classList.remove("overflow-hidden")
+    }
+    else{
+      body?.classList.add("overflow-hidden")
+    }
+  }
+
   return (
     <>
-    <nav className='flex items-center h-20 py-0 sticky top-0 justify-between bg-main-gray'>
-      <Link to={"/home"} className='h-full w-18 static md:flex hidden items-center ml-16'>
+    <nav className='flex items-center h-20 py-0 sticky top-0 justify-between bg-main-gray z-20'>
+      <Link to={"/home"} className='h-full w-18 static flex items-center ml-10'>
       <div className='h-full w-16 static flex items-center justify-center'>
-          <div className='h-16 w-16 bg-red-600 rounded-full absolute flex items-center justify-center'>
-            <img src="LogoTransparenteBlanco.png" alt="logo" className='h-14 z-10'/>
+          <div className='h-16 w-16 bg-red-600 rounded-full absolute flex items-center justify-center z-20'>
+            <img src="LogoTransparenteBlanco.png" alt="logo" className='h-14 z-20'/>
           </div>
       </div>
       </Link>
+
+      <div className='text-white text-5xl mr-16 z-20 md:hidden' onClick={ onClickEvent }>
+        <ion-icon name={open ? "close-sharp" : "menu-sharp"}></ion-icon>
+      </div>
 
       <div className='h-20 md:flex hidden justify-end md:w-1/2'>
         <Link to={"/home"} className='custom-btn'>
@@ -28,15 +48,7 @@ const NavBar = () => {
       </div>
 
       {/* Celulares */}
-      <div className={'md:hidden bg-main-gray fixed h-full w-full bottom-0 flex flex-col items-start justify-center z-10'}>
-        
-        <Link to={"/home"} className='h-20 w-full absolute top-5 z-20 flex items-center px-10'>
-        <div className='h-16 w-16 static flex items-center justify-center'>
-            <div className='h-16 w-16 bg-red-600 rounded-full absolute flex items-center justify-center'>
-              <img src="LogoTransparenteBlanco.png" alt="logo" className='h-14 z-10'/>
-            </div>
-        </div>
-        </Link>
+      <div className={`md:hidden bg-main-gray fixed h-full w-full bottom-0 z-10 flex flex-col items-start justify-center duration-500 ${open ? "left-0" : 'left-[-100%]'}`}>
 
       <Link to={"/home"} className='custom-btn'>
           <p>Home</p>
