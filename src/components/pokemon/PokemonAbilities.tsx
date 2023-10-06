2b6b9ba4f744e48af1e8a34ebbabc824df28300f
ArnulfoVargas <vargas.mejia.arnulfo04@gmail.com>
2023-10-06 03:05:18 -0600

    bugs

--- src/components/pokemon/PokemonAbilities.tsx
+++ src/components/pokemon/PokemonAbilities.tsx
@@ -1,16 +1,24 @@
 import uniqid from "uniqid"
 
+type ab = {
+    ability : {
+        name : string
+    }
+}
+
 interface Props {
     abilities : [object]
 }
 
 const PokemonAbilities = (props: Props) => {
 
+    const abilities : [ab] = props.abilities as [ab]
+
   return (
     <div className="md:w-2/3 w-full flex items-center flex-col">
         <h1 className="bg-main-red toony-borders text-white text-2xl font-titles w-fit">Abilities</h1>
         {
-            props.abilities.map((obj) => {
+            abilities.map((obj) => {
                 return (
                     <p className="font-mono-text text-lg text-secondary-gray m-1" key={uniqid()}>{obj.ability.name}</p>
                 )
--- src/components/pokemon/PokemonTypes.tsx
+++ src/components/pokemon/PokemonTypes.tsx
@@ -1,16 +1,25 @@
 import uniqid from "uniqid"
 
+type types = {
+    type: {
+        name: string
+    }
+}
+
 interface Props {
     types : [object]
 }
 
 const PokemonTypes = (props: Props) => {
+
+    const objects : [types] = props.types as [types]
+
     return (
         <div className="md:w-2/3 w-full flex items-center flex-col">
             <h1 className="bg-main-red toony-borders text-white text-2xl font-titles w-fit">Types</h1>
     
             {
-                props.types.map((obj) => {
+                objects.map((obj) => {
                     return (
                         <p className="font-mono-text text-lg text-secondary-gray m-1"
                             key={uniqid()}>{obj.type.name}</p>
